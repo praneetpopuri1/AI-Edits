a4f4bf3e4324da309fa17782f3463ec6dffc2cbc
Praneet Popuri <praneet.popuri1@gmail.com>
2026-05-27 22:12:40 -0500

    refactor json

--- inputs/in_context_jsons/json_reformater.py
+++ inputs/in_context_jsons/json_reformater.py
@@ -1,6 +1,30 @@
+#reformating timestamps to be consistent with model prompts and directions
 import json
+
+name = "clash"
+def get_seconds(x):
+    minutes = int(x)
+    seconds = round((x - minutes) * 100)
+    return minutes * 60 + seconds
+
 json_file = "blah"
 with open(json_file) as f:
     data = json.load(f)
 
-data["parts"][]
+start_vid = data["parts"][0]["start_sec"]
+start_seconds = get_seconds(start_vid)
+
+for part in data["parts"]:
+    start = get_seconds(part["start_sec"]) - start_seconds
+    end = get_seconds(part["end_sec"]) - start_seconds
+    part["start_sec"] = start
+    part["end_sec"] = end
+
+with open(name + '_fixed.json', 'w') as f:
+    json.dump(data,f)
+
+
+
+
+
+
